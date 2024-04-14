f6203491528f780fd18ec92db11845f2c0447e73
onkar-kale-ok <96816723+onkar-kale-ok@users.noreply.github.com>
2024-04-14 12:12:15 +0530

    Update pages.service.ts

--- src/pages/pages.service.ts
+++ src/pages/pages.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePageDto } from './dto/create-page.dto';
 import { UpdatePageDto } from './dto/update-page.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Page } from './entities/page.entity';
 
 @Injectable()
 export class PagesService {
+    constructor(
+    @InjectRepository(Page) private readonly pageRepository: Repository<Page>,
+  ) {}
+  
   create(createPageDto: CreatePageDto) {
     return 'This action adds a new page';
+  }
+
+      async getAllPages(): Promise<any> {
+    const result = await this.pageRepository.find();
+    return {
+      statusCode: 200,
+      message: 'User list',
+      users: result,
+    };
   }
 
   findAll() {
