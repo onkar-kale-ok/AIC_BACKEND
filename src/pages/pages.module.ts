5a4e7a6ccb3ccbe973bf60bc7b3e547f0f94d470
onkar-kale-ok <96816723+onkar-kale-ok@users.noreply.github.com>
2024-04-14 12:10:49 +0530

    Update pages.module.ts

--- src/pages/pages.module.ts
+++ src/pages/pages.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PagesService } from './pages.service';
 import { PagesController } from './pages.controller';
+import { Page } from './entities/page.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Page])],
   controllers: [PagesController],
   providers: [PagesService],
+  exports: [PagesService],
 })
 export class PagesModule {}
